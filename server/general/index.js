cb93bef2bdf18761c8c94dc3fa09596e8a2b96ea
Ivan <ivan.spiridonov.01@inbox.ru>
2022-09-01 15:19:26 +0300

    add DB importer

--- server/general/index.js
+++ server/general/index.js
@@ -14,4 +14,28 @@ module.exports = function(app, upload) {
         })
         res.json({ok: true})
     })
+
+    app.get('/import-db', async (req, res) => {
+        const Importer = require('mysql-import')
+
+        const connection = {
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'dentalclick',
+        }
+        const importer = new Importer(connection)
+         
+        importer.onProgress(progress=>{
+            var percent = Math.floor(progress.bytes_processed / progress.total_bytes * 10000) / 100
+            console.log(`${percent}% Completed`)
+        })
+        importer.import('./dump.sql').then(()=>{
+            var files_imported = importer.getImported()
+            console.log(`${files_imported.length} SQL file(s) imported.`)
+            res.json({ok: true})
+        }).catch(err=>{
+            console.error(err)
+        })
+    })
 }
